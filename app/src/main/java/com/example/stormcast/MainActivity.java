266cdd148674ae2cb867eebaa8942700249a5316
Harshit Bilkhiwal <bilkhiwalharshit@gmail.com>
2026-03-23 19:53:06 +0530

    Implemented edge-to-edge support

--- app/src/main/java/com/example/stormcast/MainActivity.java
+++ app/src/main/java/com/example/stormcast/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -15,52 +14,58 @@ import com.example.stormcast.ui.Fragment_forecast;
 import com.example.stormcast.ui.Fragment_weather;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    FrameLayout frame ;
+    FrameLayout frame;
     BottomNavigationView btnav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            return insets;
+        });
 
         frame = findViewById(R.id.frame);
         btnav = findViewById(R.id.btnav);
 
-        //default fragment
-        loadfrag(new Fragment_weather(),0);
+        // default fragment
+        loadfrag(new Fragment_weather(), 0);
 
         btnav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
 
-                if (id==R.id.nav_weather){
-                    loadfrag(new Fragment_weather(),1);
-                }
-                else if (id==R.id.nav_forecast) {
-                    loadfrag(new Fragment_forecast(),1);
+                if (id == R.id.nav_weather) {
+                    loadfrag(new Fragment_weather(), 1);
+                } else if (id == R.id.nav_forecast) {
+                    loadfrag(new Fragment_forecast(), 1);
                 }
 
                 return true;
             }
         });
 
-
-
-
     }
 
-    public void loadfrag(Fragment fragment,int flag){
+    public void loadfrag(Fragment fragment, int flag) {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
 
-        if (flag==0) {
+        if (flag == 0) {
             ft.add(R.id.frame, fragment);
-        }
-        else {
+        } else {
             ft.replace(R.id.frame, fragment);
         }
         ft.commit();
